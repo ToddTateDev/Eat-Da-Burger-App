4e20b300f3e8e31372af6c316afa7cca04e3fca1
Todd Tate <toddtatevw@gmail.com>
2019-04-10 19:59:58 -0600

    Initial Upload
    Adding most of the initial files

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers
+    (burger_name, devoured)
+VALUES
+    ("PB & J Burger", false),
+    ("Sushi Burger", false),
+    ("Surf & Turf Burger", false),
+    ("Rueben Burger", false),
+    ("Caprese Burger", false),
+    ("Poutine Burger", false);
